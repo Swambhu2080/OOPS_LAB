c6c72bbd33fbfc9fb181e570384981431fa545ef
Swambhu Mullick <71382209+Swambhu2080@users.noreply.github.com>
2020-09-17 20:11:49 +0530

    Update decnum.java

--- LAB_3/decnum.java
+++ LAB_3/decnum.java
@@ -3,10 +3,10 @@ import java.lang.*;
 class decnum{
   public static void main(String[] args) {
     int j;  
-    Double arr[] = new Double[];
+    Double arr[] = new Double[4];
     for(j=0;j<4;j++)
       {
-        arr[i]=Double.parseDouble(args[i]);
+        arr[j]=Double.parseDouble(args[j]);
       }
     Arrays.sort(arr);
     for(j=0;j<4;j++)
@@ -14,7 +14,7 @@ class decnum{
         String str = String.format("%.2f", arr[j]); 
         System.out.println(" "+str);
       }
-     String str1 = String.format("%.2f", Double.sum(Double.sum(a,b), Double.sum(c,d)));
+     String str1 = String.format("%.2f", Double.sum(Double.sum(arr[0],arr[1]), Double.sum(arr[2],arr[3])));
      System.out.println("the Sum of the inputs is : "+str1);
   }
 }
