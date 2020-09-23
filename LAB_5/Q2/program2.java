01b9b2c832114e0d31e1452f00063121bcdc14bc
Swambhu Mullick <71382209+Swambhu2080@users.noreply.github.com>
2020-09-23 13:47:43 +0530

    Update program2.java

--- LAB_5/Q2/program2.java
+++ LAB_5/Q2/program2.java
@@ -2,7 +2,7 @@ public class program2 {
     public static void main(String[] args) {
     	String name="Swambhu";
         Person p=new Person(name);
-        Student s=new Student(name);
+        Student s=new Student(name);// or p.getName()
         CSEStudent c=new CSEStudent(name);
        	p.Introduce();
        	s.Introduce();
